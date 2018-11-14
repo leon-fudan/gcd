b6478847d249eb564b75d1e3e265ea9f00c43825
Weihang <weihang@synopsys.com>
2018-11-14 10:01:59 +0800

    update 28nm sythesis reports for gcd_direct

--- syn/syn.tcl
+++ syn/syn.tcl
@@ -1,19 +1,21 @@
 
+set_host_options -max_cores 8
 set DESIGN_NAME                   "gcd"  ;#  The name of the top-level design
-set DESIGN_REF_DATA_PATH           "/remote/ailab1/weihang/course/dsp_vsli/gcd"
 set link_library        "
-        ${DESIGN_REF_DATA_PATH}/syn/library/std/db/ts16ncfslogl20hdh090f_ssgnp0p72vn40c.db
-        ${DESIGN_REF_DATA_PATH}/syn/library/std/db/ts16ncfslogl20hdl090f_ssgnp0p72vn40c.db
+        ./library/std/db/ts28nphslogl35hdh140f_ssgwc0p81v125c.db
+        ./library/std/db/ts28nphslogl35hdl140f_ssgwc0p81v125c.db
         "
 set target_library          "
-        ${DESIGN_REF_DATA_PATH}/syn/library/std/db/ts16ncfslogl20hdh090f_ssgnp0p72vn40c.db
-        ${DESIGN_REF_DATA_PATH}/syn/library/std/db/ts16ncfslogl20hdl090f_ssgnp0p72vn40c.db
+        ./library/std/db/ts28nphslogl35hdh140f_ssgwc0p81v125c.db
+        ./library/std/db/ts28nphslogl35hdl140f_ssgwc0p81v125c.db
         "
-read_verilog  "${DESIGN_REF_DATA_PATH}/rtl/gcd.v "
+read_verilog  "../rtl/gcd.v "
 current_design ${DESIGN_NAME}
 create_clock -name clk -period 0.5 clk
 set_ideal_network resetn
-check_design
-compile
+check_design > rpts/check_design.rpt
+compile_ultra
 report_area > rpts/area.rpt
 report_qor > rpts/qor.rpt
+report_power > rpts/power.rpt
+write -hierarchy -format ddc -output out.ddc
